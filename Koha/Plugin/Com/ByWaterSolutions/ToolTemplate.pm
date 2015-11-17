e367849ecf644817c03e7ce81d7a513125054de1
Barton Chittenden <bartonski@gmail.com>
2015-11-16 23:44:42 -0500

    Remove report code.

---
+++ Koha/Plugin/Com/ByWaterSolutions/ToolTemplate.pm
@@ -0,0 +1,149 @@
+package Koha::Plugin::Com::ByWaterSolutions::ToolTemplate;
+
+## It's good practive to use Modern::Perl
+use Modern::Perl;
+
+## Required for all plugins
+use base qw(Koha::Plugins::Base);
+
+## We will also need to include any Koha libraries we want to access
+use C4::Context;
+use C4::Branch;
+use C4::Members;
+use C4::Auth;
+
+## Here we set our plugin version
+our $VERSION = 0.00;
+
+## Here is our metadata, some keys are required, some are optional
+our $metadata = {
+    name   => 'ToolTemplate',
+    author => 'YOUR NAME HERE',
+    description =>
+'Starting point for writing your own tool plugin.',
+    date_authored   => '1970-01-01',
+    date_updated    => '1970-01-01',
+    minimum_version => '3.0100107',
+    maximum_version => undef,
+    version         => $VERSION,
+};
+
+## This is the minimum code required for a plugin's 'new' method
+## More can be added, but none should be removed
+sub new {
+    my ( $class, $args ) = @_;
+
+    ## We need to add our metadata here so our base class can access it
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
+
+    ## Here, we call the 'new' method for our base class
+    ## This runs some additional magic and checking
+    ## and returns our actual $self
+    my $self = $class->SUPER::new($args);
+
+    return $self;
+}
+
+## The existance of a 'tool' subroutine means the plugin is capable
+## of running a tool. The difference between a tool and a report is
+## primarily semantic, but in general any plugin that modifies the
+## Koha database should be considered a tool
+sub tool {
+    my ( $self, $args ) = @_;
+
+    my $cgi = $self->{'cgi'};
+
+    unless ( $cgi->param('submitted') ) {
+        $self->tool_step1();
+    }
+    else {
+        $self->tool_step2();
+    }
+
+}
+
+## If your tool is complicated enough to needs it's own setting/configuration
+## you will want to add a 'configure' method to your plugin like so.
+## Here I am throwing all the logic into the 'configure' method, but it could
+## be split up like the 'report' method is.
+sub configure {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    unless ( $cgi->param('save') ) {
+        my $template = $self->get_template({ file => 'configure.tt' });
+
+        ## Grab the values we already have for our settings, if any exist
+        $template->param(
+            foo => $self->retrieve_data('foo'),
+            bar => $self->retrieve_data('bar'),
+        );
+
+        print $cgi->header();
+        print $template->output();
+    }
+    else {
+        $self->store_data(
+            {
+                foo                => $cgi->param('foo'),
+                bar                => $cgi->param('bar'),
+                last_configured_by => C4::Context->userenv->{'number'},
+            }
+        );
+        $self->go_home();
+    }
+}
+
+## This is the 'install' method. Any database tables or other setup that should
+## be done when the plugin if first installed should be executed in this method.
+## The installation method should always return true if the installation succeeded
+## or false if it failed.
+sub install() {
+    my ( $self, $args ) = @_;
+
+    my $table = $self->get_qualified_table_name('mytable');
+
+    return C4::Context->dbh->do( "
+        CREATE TABLE  $table (
+            `borrowernumber` INT( 11 ) NOT NULL
+        ) ENGINE = INNODB;
+    " );
+}
+
+## This method will be run just before the plugin files are deleted
+## when a plugin is uninstalled. It is good practice to clean up
+## after ourselves!
+sub uninstall() {
+    my ( $self, $args ) = @_;
+
+    my $table = $self->get_qualified_table_name('mytable');
+
+    return C4::Context->dbh->do("DROP TABLE $table");
+}
+
+sub tool_step1 {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    my $template = $self->get_template({ file => 'tool-step1.tt' });
+
+    print $cgi->header();
+    print $template->output();
+}
+
+sub tool_step2 {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    my $template = $self->get_template({ file => 'tool-step2.tt' });
+
+    my @victims;
+    
+    $template->param( 'victims' => \@victims );
+
+    print $cgi->header();
+    print $template->output();
+}
+
+1;
